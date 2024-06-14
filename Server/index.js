95aca999450899d326744cb774caaf9c9e056996
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-14 19:53:21 +0530

    fixed

--- Server/index.js
+++ Server/index.js
@@ -3,8 +3,12 @@ const app=express();
 const connectDB=require('./Config/Database.js')
 const cors=require('cors')
 connectDB()
+const path = require('path');
+
 app.use(cors())
 app.use(express.json())
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 const PORT=8000||process.env.PORT
 app.listen(PORT,()=>{
     try {
