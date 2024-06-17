ccba95f197e87dfab1a27e98113d5078e383c21e
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-17 19:49:49 +0530

    updated backed

--- Server/Routes/ListingRoute.js
+++ Server/Routes/ListingRoute.js
@@ -5,6 +5,16 @@ const app=express();
 const multer=require('multer')
 const router=express.Router();
 app.use(express.json());
+// storage 
+const storage=multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'/public/uploads')
+    },
+    filename:function(req,file,cb){
+        cb(null,file.originalname)
+    }
+})
+const upload=multer({storage})
 router.get('/',async (req,res)=>{
     try {
         const ListingCategory=await Listings.find();
@@ -12,6 +22,64 @@ router.get('/',async (req,res)=>{
         
     } catch (error) {
         
+    }
+})
+router.post('/category',upload.array("listingPhotos"),async(req,res)=>{
+    try {
+        const {
+            creator,
+            category,
+            type,
+            streetAddress,
+            aptSuite,
+            city,
+            province,
+            country,
+            guestCount,
+            bedroomCount,
+            bedCount,
+            bathroomCount,
+            amenities,
+            title,
+            description,
+            highlight,
+            highlightDesc,
+            price,
+          } = req.body;
+         const listingPhotos=req.files
+         if (!listingPhotos) {
+            return res.status(400).send("No file uploaded.")
+         }
+         const listingPhotoPaths=listingPhotos.map((file)=>file.path)
+         const newListing = new Listing({
+            creator,
+            category,
+            type,
+            streetAddress,
+            aptSuite,
+            city,
+            province,
+            country,
+            guestCount,
+            bedroomCount,
+            bedCount,
+            bathroomCount,
+            amenities,
+            listingPhotoPaths,
+            title,
+            description,
+            highlight,
+            highlightDesc,
+            price,
+          })
+      
+          await newListing.save()
+          res.status(200).json(newListing)
+        
+    } catch (error) {
+        
+        console.log("There is something wrong ",error)
+        res.status(400).json({message:'There is an error'})
     }
 })
 module.exports=router;
