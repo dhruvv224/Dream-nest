481223aab9cdaf31fa402fdc7266a5fc7bec887f
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-21 23:44:45 +0530

    .

--- Server/Routes/ListingRoute.js
+++ Server/Routes/ListingRoute.js
@@ -11,7 +11,7 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 // storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, 'public', 'uploads'));
+        cb(null, "public/Uploads/");
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
