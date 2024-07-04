3f0db204a20510ae20c56379c878fc43036be632
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-07-04 13:55:48 +0530

    changes

--- Server/Routes/BookingRoute.js
+++ Server/Routes/BookingRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Booking = require("../Models/Booking");
+const nodemailer= require("nodemailer");
 
 router.post("/create", async (req, res) => {
     try {
